322213ab08ec18ad8d19fb0b07a21dca4eb98d91
Jakob Mueller <jakob.mueller@ibot3.de>
2021-12-13 16:42:46 +0100

    Fix url

--- backend/keywi/api/__init__.py
+++ backend/keywi/api/__init__.py
@@ -11,8 +11,7 @@ from model.helper import init_data
 from model.session import session
 
 app = FastAPI(title="Keywi", version="0.0.1", root_path=app_config.get('general', 'root_path'),
-              servers=[{"url": "{}{}".format(app_config.get('general', 'url'),
-                                             app_config.get('general', 'root_path')),
+              servers=[{"url": app_config.get('general', 'url'),
                         "description": "main"}])
 app.add_middleware(DBSessionMiddleware, db_url=app_config.get('database', 'url'))
 
