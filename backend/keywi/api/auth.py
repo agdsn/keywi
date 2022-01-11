57af50fc4c9a09b0a3f8f37dd6572de2c88af26f
Jakob Mueller <jakob.mueller@ibot3.de>
2022-01-11 11:16:09 +0100

    Use jwt token

--- backend/keywi/api/auth.py
+++ backend/keywi/api/auth.py
@@ -1,32 +1,73 @@
+from datetime import timedelta, datetime
+from typing import Optional
+
 from fastapi import Depends, HTTPException, status, APIRouter
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi_sqlalchemy import db
+from jose import jwt, JWTError
+from pydantic import BaseModel
 
+from lib.app_config import app_config
 from model import User
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+SECRET_KEY = app_config.get('general', 'jwt_secret')
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_DAYS = 30
 
-async def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
-    user = db.session.query(User).filter_by(login=token).one_or_none()
 
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+class TokenData(BaseModel):
+    login: Optional[str] = None
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        login: str = payload.get("sub")
+        if login is None:
+            raise credentials_exception
+        token_data = TokenData(login=login)
+    except JWTError:
+        raise credentials_exception
+
+    user = db.session.query(User).filter_by(login=token_data.login).one_or_none()
+
+    if user is None:
+        raise credentials_exception
     return user
 
 
 @router.post("/token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+async def get_token(form_data: OAuth2PasswordRequestForm = Depends()):
     user = db.session.query(User).filter_by(login=form_data.username).scalar()
     if not user:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
 
-    return {"access_token": user.login, "token_type": "bearer"}
+    access_token_expires = timedelta(days=ACCESS_TOKEN_EXPIRE_DAYS)
+    access_token = create_access_token(
+        data={"sub": user.login}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
