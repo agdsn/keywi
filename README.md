4461f9a34bc2785d35e27501d64011910745b941
Jakob Mueller <jakob.mueller@ibot3.de>
2022-03-09 09:47:55 +0100

    Update frontend build

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ FastAPI
 
 # Frontend
 VueJS
+
+### Build
+`npm run build`
+
+Set `VUE_APP_KEYWI_API_URL` (env) for correct backend URL.
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const { ProvidePlugin } = require('webpack');
 
 module.exports = defineConfig({
+  outputDir: "../backend/web",
   transpileDependencies: [
     'vuetify'
   ],
