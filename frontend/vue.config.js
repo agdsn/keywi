e5ce16582b7c3d0afd47e105b93391de0dd9db71
Jakob Mueller <jakob.mueller@ibot3.de>
2022-03-09 09:55:57 +0100

    Fix build

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const { ProvidePlugin } = require('webpack');
 
 module.exports = defineConfig({
-  outputDir: "../backend/web",
+  outputDir: "../backend/keywi/web",
   transpileDependencies: [
     'vuetify'
   ],
